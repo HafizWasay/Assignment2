36f22e0574b938ecb026dae7024ce2dfb8d6caf6
HafizWasay <165426228+HafizWasay@users.noreply.github.com>
2024-03-30 19:20:02 -0700

    Add files via upload

---
+++ app/page.tsx
@@ -0,0 +1,26 @@
+"use client"
+import { Navbar } from "./components/Nacbar";
+import { Hero } from "./components/Hero";
+
+import { Features } from "./components/Features";
+import { FAQ } from "./components/FAQ";
+import { Pricing } from "./components/Pricing";
+import { CTA } from "./components/CTA";
+import { Footer } from "./components/Footer";
+
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      <div className="px-[20px] lg:container lg:px-20 mx-auto">
+      <Features />
+      <FAQ />
+      <Pricing />
+      <CTA />
+      <Footer />
+      </div>
+      
+    </>
+  );
+}
